5575450c9b61a6f54adead4f846ea35ca06612d4
LEEYONGJAE <ionsi1103@gmail.com>
2024-10-04 18:51:45 +0900

    [New] Enemy Object
    Enemy 오브젝트가 원래 없었나 여튼 추가함

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject expMarble;
+    private float hp = 1f;
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Weapon")
+        {
+            Weapon weapon = other.gameObject.GetComponent<Weapon>();
+            hp -= weapon.damage;
+            if(hp <= 0)
+            {
+                Destroy(gameObject);
+                Instantiate(expMarble, transform.position, Quaternion.identity);
+            }
+        }
     }
 }
