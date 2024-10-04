839ecb866c90046a0aa5374a6a1c99d9e09e2a3e
LEEYONGJAE <ionsi1103@gmail.com>
2024-10-04 18:48:42 +0900

    [Debug] CamMove can chase Player
    Cam이 정상적으로 Player를 추적

--- Assets/Scripts/CamMove.cs
+++ Assets/Scripts/CamMove.cs
@@ -6,12 +6,18 @@ public class CamMove : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
+    public float smoothSpeed = 0.125f;
     void Start()
     {
-        offset = transform.position - player.position;
-    }
-    void LateUpdate()
-    {
         transform.position = player.position + offset;
+    }
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        
+        // 부드럽게 이동
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        
+        transform.position = smoothedPosition;
     }
 }   
