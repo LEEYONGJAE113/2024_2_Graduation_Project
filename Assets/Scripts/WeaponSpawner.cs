bb5feea1b7990eaf3785bed2c6d4efa92b33e83f
DESKTOP-SCTFQSJ\A <ionsi1103@gmail.com>
2024-10-06 19:06:52 +0900

    [New] Create WeaponSpawner Script
    WeaponSpawner 스크립트만 만들었음. 아직 오브젝트 xx

--- Assets/Scripts/WeaponSpawner.cs
+++ Assets/Scripts/WeaponSpawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class WeaponSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject[] weapons;
+
+    [SerializeField]
+    private float spawnInterval = 5f;
+
     void Start()
+    {
+        StartCoroutine(SpawnWeapons());
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnWeapons()
     {
-        
+        while (true)
+        {
+            Instantiate(weapons[0], transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnInterval);
+        }
     }
 }
