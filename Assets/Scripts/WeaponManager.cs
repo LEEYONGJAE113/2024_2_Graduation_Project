090b7a5562eb246f7ace090b93e31bf9e863f595
LEEYONGJAE <ionsi1103@gmail.com>
2024-10-30 11:20:34 +0900

    [Update] player에 스캐너 컴포넌트 받아오도록, 웨폰 init과 firebullet 추가
    아직 웨폰 부분미구현

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    public int id;
+    public int prefabsId;
     public float damage;
 
-    private Player _player;
+    public Player player;
 
-    private Rigidbody2D _rb;
 
     void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        damage = 1f;
+        player = GameManager.instance.player;
+    }
+
+    public void Init()
+    {
+        transform.parent = player.transform;
+        transform.localPosition = Vector3.zero;
+    }
+
+    void FireBullet()
+    {
+        if (!player.scanner.nearestTarget) { return; }
+
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir = targetPos - transform.position;
+        dir = dir.normalized;
+
+        // Get(temp), need item data-prefabId
+        Transform bullet = GameManager.instance.pool.Get(1).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        // need getcomponent
     }
 
 }
