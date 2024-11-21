bf4d63d799f0b9bd4014f0b52ee8314f7a4c9523
LEEYONGJAE <ionsi1103@gmail.com>
2024-11-21 15:32:51 +0900

    [Update] 아이템박스 쓸데없는거 좀 지움

--- Assets/Scripts/ItemBox.cs
+++ Assets/Scripts/ItemBox.cs
@@ -5,29 +5,12 @@ using UnityEngine;
 public class ItemBox : MonoBehaviour
 {
     private DropItemData[] _datas;
-    private bool _isBox;
-    private Collider2D _collider;
-
-    void Awake()
-    {
-        _collider = GetComponent<Collider2D>();
-    }
-
-
-    void OnEnable()
-    {
-        _isBox = true;
-    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Weapon") && _isBox)
+        if (collision.CompareTag("Weapon"))
         {
-            _isBox = false;
-        }
-        if (collision.CompareTag("Player") && !_isBox)
-        {
-            Debug.Log("Wow");
+            
         }
     }
 
